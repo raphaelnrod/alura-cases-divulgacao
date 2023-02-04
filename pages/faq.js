7d740e009e6188231849a42b2d2ecf63f6be18ad
raphaelnrod <raphaelnrod@gmail.com>
2023-02-03 23:59:18 -0300

    iniciando no nextjs

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Alura Cases - FAQ Page</h1>
+      <Link href="/">
+        Ir para Home
+      </Link>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from "../src/components/Link";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Alura Cases - HomePage</h1>
+      <Link href="/faq">
+        Ir para FAQ
+      </Link>
+    </div>
+  );
+}
+
+export default HomePage;
